7599edabf0acacac2769bea5aba61399de19d436
BaptisS <56864846+BaptisS@users.noreply.github.com>
2021-02-15 13:04:55 +0100

    Update waf_nsg_rule_443.sh

--- waf_nsg_rule_443.sh
+++ waf_nsg_rule_443.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 #https://docs.oracle.com/en-us/iaas/api/#/en/iaas/20160918/datatypes/AddSecurityRuleDetails
-echo "  {" >> nsg-waf-TCP443.json
-echo "        \"description\": null,">> nsg-waf-TCP443.json
-echo "        \"destination\": null,">> nsg-waf-TCP443.json
-echo "        \"destination-type\": null,">> nsg-waf-TCP443.json
-echo "        \"direction\": \"INGRESS\",">> nsg-waf-TCP443.json
-echo "        \"icmp-options\": null,">> nsg-waf-TCP443.json
-echo "        \"is-stateless\": false,">> nsg-waf-TCP443.json 
-echo "        \"protocol\": \"6\",">> nsg-waf-TCP443.json
-echo "        \"source\": $1,">> nsg-waf-TCP443.json
-echo "        \"source-type\": \"CIDR_BLOCK\",">> nsg-waf-TCP443.json
-echo "        \"tcp-options\": {">> nsg-waf-TCP443.json
-echo "          \"destination-port-range\": {">> nsg-waf-TCP443.json
-echo "            \"max\": 443,">> nsg-waf-TCP443.json
-echo "            \"min\": 443">> nsg-waf-TCP443.json
-echo "          },">> nsg-waf-TCP443.json
-echo "          \"source-port-range\": null">> nsg-waf-TCP443.json
-echo "        },">> nsg-waf-TCP443.json
-echo "        \"udp-options\": null">> nsg-waf-TCP443.json
-echo "      },">> nsg-waf-TCP443.json
+echo "  {" >> waf_nsg_443.json
+echo "        \"description\": null,">> waf_nsg_443.json
+echo "        \"destination\": null,">> waf_nsg_443.json
+echo "        \"destination-type\": null,">> waf_nsg_443.json
+echo "        \"direction\": \"INGRESS\",">> waf_nsg_443.json
+echo "        \"icmp-options\": null,">> waf_nsg_443.json
+echo "        \"is-stateless\": false,">> waf_nsg_443.json 
+echo "        \"protocol\": \"6\",">> waf_nsg_443.json
+echo "        \"source\": $1,">> waf_nsg_443.json
+echo "        \"source-type\": \"CIDR_BLOCK\",">> waf_nsg_443.json
+echo "        \"tcp-options\": {">> waf_nsg_443.json
+echo "          \"destination-port-range\": {">> waf_nsg_443.json
+echo "            \"max\": 443,">> waf_nsg_443.json
+echo "            \"min\": 443">> waf_nsg_443.json
+echo "          },">> waf_nsg_443.json
+echo "          \"source-port-range\": null">> waf_nsg_443.json
+echo "        },">> waf_nsg_443.json
+echo "        \"udp-options\": null">> waf_nsg_443.json
+echo "      },">> waf_nsg_443.json
